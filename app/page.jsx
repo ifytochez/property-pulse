32e17420d46ac82676f865f8e56b4d65b659bade
Tochi Ifeanyi-Okoronkwo <Tochi.Ifeanyi-Okoronkwo@interswitchgroup.com>
2024-08-15 16:36:36 +0100

    created layout, homepage and styles

---
+++ app/page.jsx
@@ -0,0 +1,12 @@
+import { title } from "process";
+import React from "react";
+
+export const metadata = {
+  title: "Home Page"
+};
+
+const HomePage = () => {
+  return <div>HomePage</div>;
+};
+
+export default HomePage;
